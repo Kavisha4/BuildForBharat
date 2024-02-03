3c80b85bc648c404642a3fa6cf075efb62037bd5
Hans Pattnaik <pattnaikhans@gmail.com>
2024-02-03 20:59:56 +0530

    updated backend

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,19 @@
-//backend code comes here
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Backend is here!");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`The app is running on port ${PORT}`);
+});
