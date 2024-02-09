edefa7019a0f87f26ca9ef3dabfdb2ad2c1a2dd4
Krishna Mehta <66839875+cybraia@users.noreply.github.com>
2024-02-09 10:40:28 +0530

    Update Form.jsx

--- frontend/src/Components/Form.jsx
+++ frontend/src/Components/Form.jsx
@@ -1,37 +1,46 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Form({ onSubmit }) {
-  const [pincode, setPincode] = useState('');
+
+function Form() {
+  const [pincodes, setPincodes] = useState('');
+  const [pincodeError, setPincodeError] = useState('');
+  const navigateTo = useNavigate();
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit(pincode);
+    const pincodes2 = pincodes.split(',').map((pc) => pc.trim());
+
+    const invalidPincode = pincodes2.find((pc) => isNaN(pc) || pc.length !== 6);
+    if (invalidPincode) {
+      setPincodeError('Pincode must be a 6-digit numerical value separated by commas.');
+      return;
+    }
+    else{
+      const pincodeArray = pincodes.split(',').map(pincode => pincode.trim());
+      navigateTo(`/results/${pincodeArray.join(',')}`);
+    }
+   
   };
 
   return (
-    <div className="flex items-center justify-center h-full">
-      <form onSubmit={handleSubmit} className="bg-gray-200 p-12 rounded-lg shadow-md z-10 backdrop-blur-md">
-        <div className="grid grid-cols-2">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="pincode">
-              Pincodes: (comma separated)
-            </label>
-            <input
-              id="pincode"
-              type="text"
-              value={pincode}
-              onChange={(e) => setPincode(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
-          <div className="mt-7 ml-4">
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white w-40 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Submit
-            </button>
-          </div>
+    <div className="relative h-screen">
+      {/* Form content */}
+      <form onSubmit={handleSubmit} className="flex items-center justify-center h-full">
+        <div className='bg-gray-300 p-10 rounded-md'> 
+          <label htmlFor="pincodes" className="block text-gray-700 text-sm font-bold mb-2">Pincodes: </label>
+          <input
+          required
+            id="pincodes"
+            type="text"
+            value={pincodes}
+            onChange={(e) => setPincodes(e.target.value)}
+            className="shadow appearance-none border rounded w-64 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Comma separated..."
+          />
+           {pincodeError && <p className="text-red-500 text-xs italic">{pincodeError}</p>}
+          <button type="submit" className="bg-blue-500 ml-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">Submit</button>
         </div>
       </form>
     </div>
