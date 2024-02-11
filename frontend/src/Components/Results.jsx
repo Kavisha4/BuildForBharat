a13a7feea66cd6469df98af9abafd5e5b7d96b91
Krishna Mehta <66839875+cybraia@users.noreply.github.com>
2024-02-11 12:29:17 +0530

    Update Results.jsx

--- frontend/src/Components/Results.jsx
+++ frontend/src/Components/Results.jsx
@@ -17,7 +17,7 @@ function Results() {
       try {
         const pincodeArray = pincodes.split(',').map(pincode => pincode.trim());
         const promises = pincodeArray.map(pincode =>
-          fetch(`http://localhost:8080/merchants?pincodes=${pincode}`)
+          fetch(`http://35.207.207.45/:8080/merchants?pincodes=${pincode}`)
             .then(response => {
               if (!response.ok) {
                 throw new Error(`Failed to fetch data for pincode ${pincode}`);
