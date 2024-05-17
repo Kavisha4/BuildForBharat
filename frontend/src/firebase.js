006797a0adbdb46c16cba41bdaf9151c817a9646
Krishna Mehta <66839875+cybraia@users.noreply.github.com>
2024-05-17 15:23:08 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,15 +1,15 @@
+// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD6YmhifirS3kmEIEBA0HwK0-13AMLyiEQ",
-    authDomain: "refined-aria-413310.firebaseapp.com",
-    projectId: "refined-aria-413310",
-    storageBucket: "refined-aria-413310.appspot.com",
-    messagingSenderId: "1095037775759",
-    appId: "1:1095037775759:web:82f48ede38762fbeeb65fa",
-    measurementId: "G-4MFJZHBFE2"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
