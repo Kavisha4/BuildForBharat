0ad2b3f57822a4ea0e1d715232d7975eafeff838
Hans Pattnaik <pattnaikhans@gmail.com>
2024-05-20 10:18:06 +0530

    Updated pincode extractor to have distinct values only

--- generators/pincode_extractor/extractor.py
+++ generators/pincode_extractor/extractor.py
@@ -2,4 +2,6 @@ import pandas as pd
  
 df = pd.read_csv("./govt_india_pincode.csv", encoding="ISO-8859-1")
 df.rename(columns = {'Pincode':'pin_code'}, inplace = True)
+unique_pincode_array = df['pin_code'].unique()
+df = pd.DataFrame({'pin_code': unique_pincode_array})
 df.to_csv('pincodes.csv', columns=['pin_code'],index=False)
