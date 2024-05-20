cba5da8c20f6ed575506fb0f45fcb74d93150194
Hans Pattnaik <pattnaikhans@gmail.com>
2024-05-21 01:01:56 +0530

    Adding cicd to update via github webhook

---
+++ cicd/main.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { exec } = require("child_process");
+
+const app = express();
+
+const PORT = 8081;
+
+app.post("/update-github", (req, res) => {
+  console.log("Req -> ", req);
+  console.log("Req Body -> ", req.body);
+  const script = exec("/bin/bash script.sh", (error, stdout, stderr) => {
+    if (stdout) console.log(stdout);
+    if (stderr) console.log(stderr);
+    if (error) console.log(`exec error: ${error}`);
+  });
+  return res.status(200).send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`The app is running on port ${PORT}`);
+});
