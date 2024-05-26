1b3b24bafd2c3c42b832c95ef669fbcc46ff1334
Krishna Mehta <66839875+cybraia@users.noreply.github.com>
2024-05-26 10:45:50 +0530

    added visualization button

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -13,6 +13,9 @@ const HomePage = () => {
             <Link to="/login" className="mt-2 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700">
               Merchant Login
             </Link>
+            <Link to="/merchant-map" className="mt-2 ml-2 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700">
+              Visualization
+            </Link>
           </div>
           <div className="mt-4 md:mt-0 md:ml-4">
             <h2 className="text-xl font-semibold mb-2">For Buyers</h2>
