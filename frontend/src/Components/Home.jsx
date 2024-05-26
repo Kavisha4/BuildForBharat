8e6f067c9e51ec78320d7c4fd9495861e4d7c8ef
Krishna Mehta <66839875+cybraia@users.noreply.github.com>
2024-05-26 21:25:38 +0530

    responsive home page

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen relative h-screen">
-      <div className="text-center p-8 bg-white rounded-lg shadow-lg w-1/5 md:w-1/2 lg:w-1/3">
+      <div className="text-center p-8 bg-white rounded-lg shadow-lg w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-1/3">
         <h1 className="text-3xl font-bold mb-6">Welcome to ONDC</h1>
         <div className="flex flex-col md:flex-row justify-around">
           <div className="mb-4 md:mb-0 md:mr-4">
